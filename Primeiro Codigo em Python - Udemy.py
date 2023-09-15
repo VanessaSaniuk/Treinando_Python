995a5fc525a8a34e69e15a91f9734fc4c96aaaa3
VANESSA SILVA DE LIMA <140930039+VanessaSaniuk@users.noreply.github.com>
2023-09-14 23:50:46 -0300

    Add files via upload

---
+++ Primeiro Codigo em Python - Udemy.py
@@ -0,0 +1,15 @@
+
+'''
+Autor:Vanessa
+Data:08/09/23
+Versão: 1.0
+Comentarios podem ser indicados com aspas triplas no início e no final
+'''
+
+#Meu primeiro código, usar hagtag para esse fim
+
+print("olá")
+print("Que dia é hoje")
+print("Tudo bem?")
+
+
